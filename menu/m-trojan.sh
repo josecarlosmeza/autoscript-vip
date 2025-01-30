e07b3c669a2b7ef58beb7488e986239f05a75c00
Longsyuncing <165179842+jvoscript@users.noreply.github.com>
2025-01-30 20:59:22 +0700

    Update m-trojan.sh

--- menu/m-trojan.sh
+++ menu/m-trojan.sh
@@ -730,61 +730,69 @@ echo "$(((bytes + 1073741823) / 1073741824)) GB"
 fi
 }
 function cek-tr(){
-clear
-xrayy=$(cat /var/log/xray/access.log | wc -l)
-if [[ xrayy -le 5 ]]; then
-systemctl restart xray
-fi
-xraylimit
-echo -e "$COLOR1╭═════════════════════════════════════════════════╮${NC}"
-echo -e "$COLOR1│${NC}${COLBG1}             ${WH}• TROJAN USER ONLINE •              ${NC}$COLOR1│ $NC"
-echo -e "$COLOR1╰═════════════════════════════════════════════════╯${NC}"
-echo -e "$COLOR1╭═════════════════════════════════════════════════╮${NC}"
-vm=($(cat /etc/xray/config.json | grep "^#trg" | awk '{print $2}' | sort -u))
-echo -n >/tmp/vm
-for db1 in ${vm[@]}; do
-logvm=$(cat /var/log/xray/access.log | grep -w "email: ${db1}" | tail -n 100)
-while read a; do
-if [[ -n ${a} ]]; then
-set -- ${a}
-ina="${7}"
-inu="${2}"
-anu="${3}"
-enu=$(echo "${anu}" | sed 's/tcp://g' | sed '/^$/d' | cut -d. -f1,2,3)
-now=$(tim2sec ${timenow})
-client=$(tim2sec ${inu})
-nowt=$(((${now} - ${client})))
-if [[ ${nowt} -lt 40 ]]; then
-cat /tmp/vm | grep -w "${ina}" | grep -w "${enu}" >/dev/null
-if [[ $? -eq 1 ]]; then
-echo "${ina} ${inu} WIB : ${enu}" >>/tmp/vm
-splvm=$(cat /tmp/vm)
-fi
-fi
-fi
-done <<<"${logvm}"
-done
-if [[ ${splvm} != "" ]]; then
-for vmuser in ${vm[@]}; do
-vmhas=$(cat /tmp/vm | grep -w "${vmuser}" | wc -l)
-tess=0
-if [[ ${vmhas} -gt $tess ]]; then
-byt=$(cat /etc/limit/trojan/${vmuser})
-gb=$(convert ${byt})
-lim=$(cat /etc/trojan/${vmuser})
-lim2=$(convert ${lim})
-echo -e "$COLOR1${NC} USERNAME : \033[0;33m$vmuser"
-echo -e "$COLOR1${NC} IP LOGIN : \033[0;33m$vmhas"
-echo -e "$COLOR1${NC} USAGE : \033[0;33m$gb"
-echo -e "$COLOR1${NC} LIMIT : \033[0;33m$lim2"
-echo -e ""
-fi
-done
-fi
-echo -e "$COLOR1╰═════════════════════════════════════════════════╯${NC}"
-echo ""
-read -n 1 -s -r -p "   Press any key to back on menu"
-m-trojan
+    clear
+    xrayy=$(cat /var/log/xray/access.log | wc -l)
+    if [[ xrayy -le 5 ]]; then
+        systemctl restart xray
+    fi
+    xraylimit
+    echo -e "$COLOR1╭═════════════════════════════════════════════════╮${NC}"
+    echo -e "$COLOR1│${NC}${COLBG1}             ${WH}• TROJAN USER ONLINE •              ${NC}$COLOR1│ $NC"
+    echo -e "$COLOR1╰═════════════════════════════════════════════════╯${NC}"
+    echo -e "$COLOR1╭═════════════════════════════════════════════════╮${NC}"
+    vm=($(cat /etc/xray/config.json | grep "^#trg" | awk '{print $2}' | sort -u))
+    echo -n >/tmp/vm
+    for db1 in ${vm[@]}; do
+        logvm=$(cat /var/log/xray/access.log | grep -w "email: ${db1}" | tail -n 100)
+        while read a; do
+            if [[ -n ${a} ]]; then
+                set -- ${a}
+                ina="${7}"
+                inu="${2}"
+                anu="${3}"
+                enu=$(echo "${anu}" | sed 's/tcp://g' | sed '/^$/d' | cut -d. -f1,2,3)
+                now=$(tim2sec ${timenow})
+                client=$(tim2sec ${inu})
+                nowt=$(((${now} - ${client})))
+                if [[ ${nowt} -lt 40 ]]; then
+                    cat /tmp/vm | grep -w "${ina}" | grep -w "${enu}" >/dev/null
+                    if [[ $? -eq 1 ]]; then
+                        echo "${ina} ${inu} WIB : ${enu}" >>/tmp/vm
+                        splvm=$(cat /tmp/vm)
+                    fi
+                fi
+            fi
+        done <<<"${logvm}"
+    done
+    if [[ ${splvm} != "" ]]; then
+        for vmuser in ${vm[@]}; do
+            vmhas=$(cat /tmp/vm | grep -w "${vmuser}" | wc -l)
+            tess=0
+            if [[ ${vmhas} -gt $tess ]]; then
+                if [[ -f /etc/limit/trojan/${vmuser} ]]; then
+                    byt=$(cat /etc/limit/trojan/${vmuser})
+                    gb=$(convert ${byt})
+                else
+                    gb="N/A"
+                fi
+                if [[ -f /etc/trojan/${vmuser} ]]; then
+                    lim=$(cat /etc/trojan/${vmuser})
+                    lim2=$(convert ${lim})
+                else
+                    lim2="N/A"
+                fi
+                echo -e "$COLOR1${NC} USERNAME : \033[0;33m$vmuser"
+                echo -e "$COLOR1${NC} IP LOGIN : \033[0;33m$vmhas"
+                echo -e "$COLOR1${NC} USAGE : \033[0;33m$gb"
+                echo -e "$COLOR1${NC} LIMIT : \033[0;33m$lim2"
+                echo -e ""
+            fi
+        done
+    fi
+    echo -e "$COLOR1╰═════════════════════════════════════════════════╯${NC}"
+    echo ""
+    read -n 1 -s -r -p "   Press any key to back on menu"
+    m-trojan
 }
 function list-trojan(){
 clear
