896f3341986fa21ba10bd78fa6932b9b85acbc05
Longsyuncing <165179842+jvoscript@users.noreply.github.com>
2025-01-31 16:31:05 +0700

    Update RUSAK 1

--- m-update.sh
+++ m-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-######=#####- COLOR CODE -########=######
+######=####- COLOR CODE -########=#####
 colornow=$(cat /etc/rmbl/theme/color.conf)
 NC="\e[0m"
 RED="\033[0;31m"
