614c558efcf4b61e4649c3d3e7495e970595adb0
josecarlosmeza <75866376+josecarlosmeza@users.noreply.github.com>
2025-03-09 12:45:18 -0300

    Update limit.sh
    Signed-off-by: josecarlosmeza <75866376+josecarlosmeza@users.noreply.github.com>

--- bin/limit.sh
+++ bin/limit.sh
@@ -7,20 +7,24 @@ wget -q -O /etc/xray/limit.vmess "${REPO}vmess" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.vless "${REPO}vless" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.trojan "${REPO}trojan" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.shadowsocks "${REPO}shadowsocks" >/dev/null 2>&1
+
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
 chmod +x /etc/xray/limit.trojan
 chmod +x /etc/xray/limit.shadowsocks
+
 systemctl daemon-reload
+
 systemctl enable --now limitvmess
 systemctl enable --now limitvless
 systemctl enable --now limittrojan
 systemctl enable --now limitshadowsocks
-# systemctl start limitvmess
-# systemctl start limitvless
-# systemctl start limittrojan
-# systemctl start limitshadowsocks
-# systemctl restart limitvmess
-# systemctl restart limitvless
-# systemctl restart limittrojan
-# systemctl restart limitshadowsocks
+
+ systemctl start limitvmess
+ systemctl start limitvless
+ systemctl start limittrojan
+ systemctl start limitshadowsocks
+ systemctl restart limitvmess
+ systemctl restart limitvless
+ systemctl restart limittrojan
+ systemctl restart limitshadowsocks
